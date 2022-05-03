9d3570a0ca429715f08a79ee917624282ea3d30d
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-05-03 13:29:53 -0400

    primeira aula - finalizado

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,24 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
+
 export default function Widget() {
-    return(
-        <h1>Ola</h1>
-    )
+
+  return(
+    <Popover className='absolute bottom-4 right-4'>
+    
+    <Popover.Panel>Hello World</Popover.Panel>
+
+      <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+
+        <ChatTeardropDots className='w-6 h-6'/>
+        
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500'>
+          <span className='pl-2'></span>
+          Feedback
+          </span>
+
+      </Popover.Button>
+    </Popover>
+    
+  )
 }
