cb412ec4554ad6c5595365761a349cee3cae7a28
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-05-03 16:02:19 -0400

    update widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,12 +1,15 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
 
 export default function Widget() {
 
   return(
-    <Popover className='absolute bottom-4 right-4'>
+    <Popover className='absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col items-end'>
     
-    <Popover.Panel>Hello World</Popover.Panel>
+    <Popover.Panel>
+      <WidgetForm />
+    </Popover.Panel>
 
       <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
 
