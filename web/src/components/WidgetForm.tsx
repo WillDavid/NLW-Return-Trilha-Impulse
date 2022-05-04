33997a23b3947a43caba4e35532bfaabc5822891
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-05-03 22:19:52 -0400

    update

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,7 +1,23 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            Hello World
+            <header>
+                <span className="text-xl leading-6">Deixe seu Feedback</span>
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+                
+
+            </div>
+
+
+            <footer className="text-xs text-neutral-400">
+                Feito com S2 pelo <a className="underline underline-offset-2" href="https://will-frontend-dev.netlify.app/#/">Will. David</a>
+
+            </footer>
         </div>
     )
 }
